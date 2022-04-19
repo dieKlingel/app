527af0e2154052ee0c338a87c3653e1fbea51301
Kai Mayer <kai.mayer@t-online.de>
2022-04-19 22:58:25 +0200

    cupertion page added

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'package:dieklingel_app/views/home.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -10,13 +12,10 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-        home: Scaffold(
-      body: Center(
-        child: Row(
-          children: const [Text("A")],
-        ),
+    return const CupertinoApp(
+      home: CupertinoPageScaffold(
+        child: Home(),
       ),
-    ));
+    );
   }
 }
--- lib/views/home.dart
+++ lib/views/home.dart
@@ -1,10 +1,23 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 
 class Home extends StatelessWidget {
   const Home({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return const CupertinoPageScaffold(child: Center(child: Text("Ja")));
+    return CupertinoPageScaffold(
+        navigationBar: const CupertinoNavigationBar(
+          middle: Text("dieKlingel"),
+        ),
+        child: Center(
+            child: Row(
+          children: const [
+            Text("124",
+                style: TextStyle(
+                  color: Colors.red,
+                ))
+          ],
+        )));
   }
 }
