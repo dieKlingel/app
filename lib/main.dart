796e17837f1e784a2979abc16c8c519b522bd065
Kai Mayer <kai.mayer@t-online.de>
2022-07-06 20:09:17 +0200

    use global shared prefrences

--- lib/main.dart
+++ lib/main.dart
@@ -1,41 +1,34 @@
+import 'package:dieklingel_app/components/connection_configuration.dart';
 import 'package:dieklingel_app/views/home_view.dart';
 import 'package:dieklingel_app/views/settings/connection_configuration_view.dart';
 import 'package:flutter/cupertino.dart';
 
 import 'package:firebase_core/firebase_core.dart';
-import 'package:flutter/foundation.dart';
-import 'package:shared_preferences/shared_preferences.dart';
 import 'firebase_options.dart';
+
+import 'globals.dart' as app;
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await app.init();
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
-  runApp(const MyApp());
+  runApp(const App());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+class App extends StatelessWidget {
+  const App({Key? key}) : super(key: key);
 
-  Future<bool> isInitialzied() async {
-    SharedPreferences prefs = await SharedPreferences.getInstance();
-    List<String> configurations =
-        prefs.getStringList("configuration") ?? List<String>.empty();
-    return configurations.isNotEmpty;
+  bool get isInitialzied {
+    return app.connectionConfigurations.isNotEmpty;
   }
 
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return CupertinoApp(
-      home: FutureBuilder<bool>(
-        future: isInitialzied(),
-        builder: ((context, snapshot) {
-          bool initialized = snapshot.data ?? false;
-          return initialized ? const HomeView() : ConnectionConfigurationView();
-        }),
-      ),
+      home: isInitialzied ? const HomeView() : ConnectionConfigurationView(),
     );
   }
 }
