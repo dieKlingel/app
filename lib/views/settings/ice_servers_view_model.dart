169accbe6058b3d2af9444d5efe4c61da1bc9166
Kai Mayer <kai.mayer@t-online.de>
2022-12-31 00:07:00 +0100

    👌 IMPROVE: remove object_db dependencie, because we replaced object_db with hive

--- lib/views/settings/ice_servers_view_model.dart
+++ lib/views/settings/ice_servers_view_model.dart
@@ -1,27 +1,8 @@
 import 'package:dieklingel_app/models/ice_server.dart';
 import 'package:flutter/material.dart';
-import 'package:objectdb/objectdb.dart';
-
-import '../../database/objectdb_factory.dart';
 
 class IceServersViewModel extends ChangeNotifier {
   List<IceServer> _servers = [];
-  ObjectDB? _db;
-
-  IceServersViewModel() {
-    _init();
-  }
-
-  void _init() async {
-    _db = await ObjectDBFactory.named("iceservers");
-    await _db!.remove({"uuid": null});
-    List<Map<dynamic, dynamic>> result = await _db!.find({});
-    /* _servers = result
-        .map((e) => e.cast<String, dynamic>())
-        .map((e) => IceServer.fromMap(e))
-        .toList(); */
-    notifyListeners();
-  }
 
   List<IceServer> get servers => List.unmodifiable(_servers);
 
