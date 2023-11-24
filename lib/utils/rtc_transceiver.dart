44100fa2e150fb31bea9fd3bd60e5e9565e8a508
KoiFresh <kai@mayer.sh>
2023-11-24 14:27:58 +0100

    chore: remove unused files

--- lib/utils/rtc_transceiver.dart
+++
@@ -1,8 +0,0 @@
-import 'package:flutter_webrtc/flutter_webrtc.dart';
-
-class RtcTransceiver {
-  RTCRtpMediaType kind;
-  TransceiverDirection direction;
-
-  RtcTransceiver({required this.kind, required this.direction});
-}
